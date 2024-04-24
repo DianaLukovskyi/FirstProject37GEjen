cc26c0cfd80427afade8da5fb1375a14a944e155
Dianka <lukovsky38@gmail.com>
2024-04-25 02:37:41 +0300

    homework refactoring add

---
+++ src/test/java/levelone/TestBase.java
@@ -0,0 +1,33 @@
+package levelone;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.time.Duration;
+
+public class TestBase {
+
+    static WebDriver driver;
+
+    @BeforeSuite
+    public void init() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--lang=en");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+        // implicit waiting's
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.navigate().to("https://demowebshop.tricentis.com/");
+    }
+
+    @AfterSuite
+    public void tearDown() {
+        driver.quit();
+    }
+}
