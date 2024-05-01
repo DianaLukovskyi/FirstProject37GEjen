aaf3f35730612d4cb8acd15b3c427dfd226ec3b3
Dianka <lukovsky38@gmail.com>
2024-05-01 22:05:39 +0300

    refactoring4 value project

---
+++ src/test/java/helpers/ContactHelper.java
@@ -0,0 +1,17 @@
+package helpers;
+import config.ApplicationManager;
+import dto.ContactDTO;
+import dto.UserDTO;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ContactHelper extends BaseHelper {
+    public ContactHelper(WebDriver driver) {
+        super(driver);
+    }
+
+
+
+    }
+
